300597cdb2498cedbc893cdf0302142596e82360
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-22 18:05:42 -0800

    text area

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -5,12 +5,23 @@ import java.awt.event.ActionListener;
 
 public class TextEditor extends JFrame implements ActionListener {
 
+    JTextArea textArea;
+
     TextEditor(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Text Editor");
         this.setSize(500, 500);
         this.setLayout(new FlowLayout());
         this.setLocationRelativeTo(null);
+
+        textArea = new JTextArea();
+        textArea.setPreferredSize(new Dimension(450, 450));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setFont(new Font(null, Font.BOLD, 20));
+
+
+        this.add(textArea);
         this.setVisible(true);
     }
 
